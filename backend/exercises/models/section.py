4544c229b8a84fb8cfe9e62cd65f03b0cefca90b
Antoine Jacques <antoine.jacques@outlook.be>
2020-02-25 10:07:34 +0100

    Verbose name + __str__ in models

--- backend/exercises/models/section.py
+++ backend/exercises/models/section.py
@@ -7,7 +7,13 @@ class Section(models.Model):
     parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.PROTECT)
     number = models.PositiveSmallIntegerField()
 
+    def __str__(self):
+        return "Section n°{number} ({academic_year}) - {name}".format(
+            number=self.number, academic_year=self.academic_year, name=self.name
+        )
+
     class Meta:
+        verbose_name = "section"
         unique_together = (
             "parent",
             "number",
