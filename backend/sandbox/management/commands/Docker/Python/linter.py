37d7a8fd12a65a7d4602d6050491d2ec6891d94e
Antoine Jacques <antoine.jacques@outlook.be>
2020-03-23 09:54:36 +0100

    changement de pc

--- backend/sandbox/management/commands/Docker/Python/linter.py
+++ backend/sandbox/management/commands/Docker/Python/linter.py
@@ -1,5 +1,24 @@
+import yaml
+import re
 from flake8.api import legacy as flake8
 
+with open("errors.yaml", "r") as stream:
+    try:
+        errors_codes = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+with open("warnings.yaml", "r") as stream:
+    try:
+        warnings_codes = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+french_errors_codes_keys = errors_codes["french"].keys()
+regex_french_errors = ".*|".join(french_errors_codes_keys)
+print(regex_french_errors)
+french_warnings_codes_keys = warnings_codes["french"].keys()
+regex_french_warnings = ".*|".join(french_warnings_codes_keys)
+print(regex_french_warnings)
+print(errors_codes, warnings_codes)
 style_guide = flake8.get_style_guide()
 report = style_guide.input_file("to_lint.py")
 print(report)
