3ab5b9929a7815642eb35b53abed78221d9021ee
mdallava <maxime.dallavalle@student.unamur.be>
2020-03-26 15:04:57 +0100

    default value

--- backend/bot/models.py
+++ backend/bot/models.py
@@ -8,7 +8,7 @@ from ckeditor.fields import RichTextField
 class Question(models.Model):
     intitule = models.TextField(verbose_name="question type (pas de ponctuation,majuscule)")
     matched = models.BooleanField(verbose_name="possède une réponse associée", default=True)
-    asked = models.IntegerField(verbose_name="nombre de fois que la question a été posée", default=0)
+    asked = models.IntegerField(verbose_name="nombre de fois que la question a été posée", default=1)
 
     class Meta:
         ordering = ['intitule']
