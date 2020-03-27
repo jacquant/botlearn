4b67989c4752038714f2001653d8cba2027ceca6
mdallava <maxime.dallavalle@student.unamur.be>
2020-03-27 14:31:24 +0100

    update admin select box

--- backend/bot/admin.py
+++ backend/bot/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from bot.models import Question, Reponse
 
+
+@admin.register(Reponse)
+class ResponseAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        for que in form.cleaned_data['question'].all():
+            que.matched = True
+            que.save()
+
+        super().save_model(request, obj, form, change)
+
+
 admin.site.register(Question)
-admin.site.register(Reponse)
